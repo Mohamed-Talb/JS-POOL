0d86bf37626c6b5b11c29f7b3275ff95f8a39dfe
Mohamed Taleb <mtaleb.contact@gmail.com>
2026-03-01 14:26:18 +0000

    add 7-OBJECT/10-pronoun

--- POOL-SOLUTIONS/7-OBJECT/10-pronoun/pronoun.js
+++ POOL-SOLUTIONS/7-OBJECT/10-pronoun/pronoun.js
@@ -1,12 +1,24 @@
-
 function pronoun(str)
 {
-	const pron = {};
-	let values  = str.match(/\b(you|i|he|she|it|they|we)\b(\s+(?!(you|i|he|she|it|they|we)\b)\w+)?/g);
-	values = values.map((elm)=>elm.split(/\s/));
-	
-	return values;
-
+    let pronounObj = {};
+    let currPronoun , currPronounArg;
+    let pronounInfos;
+    let regex = /\b(i|you|he|she|it|they|we)\b(\s+(?!(i|you|he|she|it|they|we)\b)\w*)?/gi;
+    let allpronoun = str.match(regex);
+    if (allpronoun === null)
+        return {};
+    allpronoun = allpronoun.map((arg) => arg.split(/\s/));
+    for (let Curr of allpronoun)
+    {
+        currPronoun = Curr[0].toLowerCase();
+        currPronounArg = Curr.slice(1,);
+        if (!Object.hasOwn(pronounObj,currPronoun))
+            pronounObj[currPronoun] = {word: [...currPronounArg], count: 1};
+        else
+        {
+            pronounObj[currPronoun].word = pronounObj[currPronoun].word.concat(currPronounArg);
+            pronounObj[currPronoun].count += 1;
+        }
+    }
+    return pronounObj;
 }
-
-console.log(pronoun("If he you want to buy something you have to pay you."));
